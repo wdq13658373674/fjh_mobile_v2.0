0e3438b94bfef2829cfb028b692371f256f1bcbc
Andy <191142742@qq.com>
2017-03-13 17:33:45 +0800

    房计划2.0移动端框架

---
+++ js/public.js
@@ -0,0 +1,47 @@
+/*
+ *设备兼容设置
+ * */
+var pixelRatio = 1 / window.devicePixelRatio;
+oMeta = document.createElement('meta');
+oMeta.charset = 'utf-8';
+oMeta.name="viewport";
+oMeta.content='width=device-width,initial-scale=' + pixelRatio + ',minimum-scale=' + pixelRatio + ',maximum-scale=' + pixelRatio;
+document.getElementsByTagName('head')[0].appendChild(oMeta);
+var html = document.getElementsByTagName("html")[0];
+var pageWidth = html.getBoundingClientRect().width;
+html.style.fontSize = pageWidth / 15 + "px";
+
+/*
+ *电话号码验证
+ * @phone 手机号码
+ * */
+function is_mobile(phone) {
+    var flag = false;
+//    var reg0 = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;   //判断 固话
+    var reg1 = /^((\(\d{2,3}\))|(\d{3}\-))?(13|15|18|14|17)\d{9}$/;                     //判断 手机
+//    if (reg0.test(phone)) {
+//        flag = true;
+//    }
+    if (reg1.test(phone)) {
+        flag = true;
+    }
+    if (!flag) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*
+ *邮箱格式验证
+ * @emails 邮箱地址
+ * */
+function is_email(emails) {
+    var reg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    var email = emails.match(reg);
+    if (email != null) {
+        return true;  //正确
+    } else {
+        return false; //错误
+    }
+}
